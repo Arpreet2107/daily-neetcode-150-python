84a543fa5ae4f6eaa866920459720aacf0519669
Arpreet <arpreet4114@gmail.com>
2026-03-09 20:25:44 +0530

    lc-217 solved

--- lc-217.py
+++ lc-217.py
@@ -7,5 +7,6 @@ class Solution:
                 return True# If it is, we have found a duplicate, so we return True
             hashset.add(num)# If the number is not in the set, we add it to the set for future reference
         return False# If we finish iterating through the list without finding any duplicates, we return False
-# Time complexity: O(nlogn) due to the set lookup and insertion operations, which on average are O(1) but can be O(n) in the worst case. However, since we are only doing a single pass through the list, the overall time complexity is O(n).
+# Time complexity: O(n) due to the set lookup and insertion operations, which on average are O(1) but can be O(n) in the worst case.
+# However, since we are only doing a single pass through the list, the overall time complexity is O(n).
 # Space complexity: O(n)
